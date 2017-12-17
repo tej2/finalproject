d5eb1a301abafe6959026bcd53d193eff5632b41
tej2 <31868523+tej2@users.noreply.github.com>
2017-12-17 09:37:45 -0500

    Update accounts.php

--- collections/accounts.php
+++ collections/accounts.php
@@ -1,1 +1,22 @@
 <?php
+
+class accounts extends \database\collection
+{
+    protected static $modelName = 'account';
+  
+    public static  function findUserbyUN($login) {
+        $tableName = get_called_class();
+        $sql = 'SELECT * FROM ' .$tableName . ' WHERE email ="' . $login.'"';
+
+        $recordsSet = self::getResults($sql);
+        if (is_null($recordsSet)) {
+            echo "False";
+            return FALSE;
+        } else {
+            echo "True";
+            return $recordsSet[0];
+        }
+      
+    }
+}
+?>
