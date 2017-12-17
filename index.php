bd4bb0ecf4590841774435884c9373f9d86f6229
tej2 <31868523+tej2@users.noreply.github.com>
2017-12-17 09:32:23 -0500

    Update index.php

--- index.php
+++ index.php
@@ -3,8 +3,59 @@
 ini_set('display_errors', 'On');
 error_reporting(E_ALL);
 
-include_once "autoload.php";
-include_once "database.php";
+define('DATABASE', 'tej2');
+define('USERNAME', 'tej2');
+define('PASSWORD', 'sCUGMmHv');
+define('CONNECTION', 'sql2.njit.edu');
+
+class Manage
+{
+    public static function core($class)
+    {
+    $path = 'core/' . str_replace('\\', '/', $class) . '.php';
+  if (is_file($path)) {
+            include $path;
+            return;
+        }
+    }
+    public static function controllers($class)
+    {
+    $path = 'controllers/' . str_replace('\\', '/', $class) . '.php';
+ if (is_file($path)) {
+            include $path;
+            return;
+        }
+    }
+    public static function models($class)
+    {
+    $path = 'models/' . str_replace('\\', '/', $class) . '.php';
+        if (is_file($path)) {
+            include $path;
+            return;
+        }
+    }
+    public static function collections($class)
+    {
+    $path = 'collections/' . str_replace('\\', '/', $class) . '.php';
+            if (is_file($path)) {
+            include $path;
+            return;
+        }
+    }
+    public static function routes($class)
+    {
+        $path = 'routes/' . str_replace('\\', '/', $class) . '.php';
+        if (is_file($path)) {
+            include $path;
+            return;
+        }
+    }
+}
+spl_autoload_register(array('Manage', 'routes'));
+spl_autoload_register(array('Manage', 'controllers'));
+spl_autoload_register(array('Manage', 'collections'));
+spl_autoload_register(array('Manage', 'models'));
+spl_autoload_register(array('Manage', 'core'));
 
 $response = http\processRequest::createResponse();
 
