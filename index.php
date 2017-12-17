37268305d196839ab627f9d982c01247e113d7b2
tej2 <31868523+tej2@users.noreply.github.com>
2017-12-17 16:39:27 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,62 +1,11 @@
 <?php
-
+//turn on debugging messages
 ini_set('display_errors', 'On');
 error_reporting(E_ALL);
-
-define('DATABASE', 'tej2');
-define('USERNAME', 'tej2');
-define('PASSWORD', 'sCUGMmHv');
-define('CONNECTION', 'sql2.njit.edu');
-
-class Manage
-{
-    public static function core($class)
-    {
-    $path = 'core/' . str_replace('\\', '/', $class) . '.php';
-  if (is_file($path)) {
-            include $path;
-            return;
-        }
-    }
-    public static function controllers($class)
-    {
-    $path = 'controllers/' . str_replace('\\', '/', $class) . '.php';
- if (is_file($path)) {
-            include $path;
-            return;
-        }
-    }
-    public static function models($class)
-    {
-    $path = 'models/' . str_replace('\\', '/', $class) . '.php';
-        if (is_file($path)) {
-            include $path;
-            return;
-        }
-    }
-    public static function collections($class)
-    {
-    $path = 'collections/' . str_replace('\\', '/', $class) . '.php';
-            if (is_file($path)) {
-            include $path;
-            return;
-        }
-    }
-    public static function routes($class)
-    {
-        $path = 'routes/' . str_replace('\\', '/', $class) . '.php';
-        if (is_file($path)) {
-            include $path;
-            return;
-        }
-    }
-}
-spl_autoload_register(array('Manage', 'routes'));
-spl_autoload_register(array('Manage', 'controllers'));
-spl_autoload_register(array('Manage', 'collections'));
-spl_autoload_register(array('Manage', 'models'));
-spl_autoload_register(array('Manage', 'core'));
-
+//Autoloader class to load all the different directories
+include_once "autoload.php";
+//put your database credentials here
+include_once "database.php";
+//this starts the program as a static.  Start tracing the program from here following the classes and methods being called
 $response = http\processRequest::createResponse();
-
 ?>
