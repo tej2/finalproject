723224e992100cf8971aaf0eeaf2a1651563101b
tej2 <31868523+tej2@users.noreply.github.com>
2017-12-17 10:34:19 -0500

    Update account.php

--- models/account.php
+++ models/account.php
@@ -1,1 +1,36 @@
-l
+<?php
+final class account extends \database\model
+{
+    public $id;
+    public $email;
+    public $fname;
+    public $lname;
+    public $phone;
+    public $birthday;
+    public $gender;
+    public $password;
+    protected static $modelName = 'account';
+    public static function getTablename()
+    {
+        $tableName = 'accounts';
+        return $tableName;
+    }
+    public static function findTasks()
+    {
+        $records = todos::findAll();
+        print_r($records);
+        return $records;
+    }
+    public function setPassword($password) {
+        $password = password_hash($password, PASSWORD_DEFAULT);
+        return $password;
+    }
+    public function checkPassword($password) {
+        echo 'checking your password'.$password;
+        $checkpsw = password_verify($password, $this->password);
+        echo 'returning';
+        print_r($checkpsw);
+        return $checkpsw;
+    }
+}
+?>
