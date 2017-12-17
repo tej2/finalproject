1dc03b1fc170cda29ba947a5e52864945ea79d52
tej2 <31868523+tej2@users.noreply.github.com>
2017-12-17 16:38:47 -0500

    Create autoload.php

---
+++ autoload.php
@@ -0,0 +1,52 @@
+<?php
+
+
+class Manage
+{
+    public static function core($class)
+    {
+    $path = 'core/' . str_replace('\\', '/', $class) . '.php';
+  if (is_file($path)) {
+            include $path;
+            return;
+        }
+    }
+    public static function controllers($class)
+    {
+    $path = 'controllers/' . str_replace('\\', '/', $class) . '.php';
+ if (is_file($path)) {
+            include $path;
+            return;
+        }
+    }
+    public static function models($class)
+    {
+    $path = 'models/' . str_replace('\\', '/', $class) . '.php';
+        if (is_file($path)) {
+            include $path;
+            return;
+        }
+    }
+    public static function collections($class)
+    {
+    $path = 'collections/' . str_replace('\\', '/', $class) . '.php';
+            if (is_file($path)) {
+            include $path;
+            return;
+        }
+    }
+    public static function routes($class)
+    {
+        $path = 'routes/' . str_replace('\\', '/', $class) . '.php';
+        if (is_file($path)) {
+            include $path;
+            return;
+        }
+    }
+}
+spl_autoload_register(array('Manage', 'routes'));
+spl_autoload_register(array('Manage', 'controllers'));
+spl_autoload_register(array('Manage', 'collections'));
+spl_autoload_register(array('Manage', 'models'));
+spl_autoload_register(array('Manage', 'core'));
+?>
