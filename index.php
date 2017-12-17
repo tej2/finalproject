4bf2989d539f50632d3056a347ce3a9988c7de66
tej2 <31868523+tej2@users.noreply.github.com>
2017-12-17 09:21:45 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
+
+include_once "autoload.php";
+include_once "database.php";
+
+$response = http\processRequest::createResponse();
+
+?>
