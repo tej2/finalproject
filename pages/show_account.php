6a20148880da91a31e3edb35a2cf93800bbe7043
tej2 <31868523+tej2@users.noreply.github.com>
2017-12-17 16:31:46 -0500

    Create show_account.php

---
+++ pages/show_account.php
@@ -0,0 +1,35 @@
+<!doctype html>
+
+<html>
+
+<body>
+
+<h1>Email: <?php echo $data->email; ?></h1>
+<h1>First Name: <?php echo $data->fname; ?></h1>
+<h1>Last Name: <?php echo $data->lname; ?></h1>
+
+
+<?php
+print_r($data);
+?>
+
+<form action="index.php?page=accounts&action=save&id=<?php echo $data->id; ?>" method="post">
+
+    First name: <input type="text" name="fname" value="<?php echo $data->fname; ?>"><br>
+
+    Last name: <input type="text" name="lname" value="<?php echo $data->lname; ?>"><br>
+    Email: <input type="text" name="email" value="<?php echo $data->email; ?>"><br>
+    Phone: <input type="text" name="phone" value="<?php echo $data->phone; ?>"><br>
+    Birthday: <input type="text" name="birthday" value="<?php echo $data->birthday; ?>"><br>
+    Gender: <input type="text" name="gender" value="<?php echo $data->gender; ?>"><br>
+    <input type="submit" value="Submit form">
+</form>
+
+
+<form action="index.php?page=accounts&action=delete&id=<?php echo $data->id; ?> " method="post" id="form1">
+    <button type="submit" form="form1" value="delete">Delete</button>
+</form>
+
+
+</body>
+</html>
