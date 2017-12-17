3f477db8e0ec8df8bacb718f28fc31e724a21d68
tej2 <31868523+tej2@users.noreply.github.com>
2017-12-17 09:49:53 -0500

    Create controllerforhomepage.php

---
+++ controllers/controllerforhomepage.php
@@ -0,0 +1,14 @@
+<?php
+class controllerforhomepage extends http\controller
+{
+    public static function show()
+    {
+        $templateData['site_name'] = 'Tiffany Johnson - IS 601 Final Project';
+        self::getTemplate('homepage', $templateData);
+    }
+    public static function create()
+    {
+        print_r($_POST);
+    }
+}
+?>
