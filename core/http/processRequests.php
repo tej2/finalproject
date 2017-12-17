3ccdd2e44aca65be9f7792d23b889d5366289240
tej2 <31868523+tej2@users.noreply.github.com>
2017-12-17 10:12:17 -0500

    Create processRequests.php

---
+++ core/http/processRequests.php
@@ -0,0 +1,33 @@
+<?php
+namespace http;
+class processRequest
+{
+    public static function createResponse()
+    {
+        $requested_route = processRequest::getRequestedRoute();
+        $controller_name = $requested_route->controller;
+        $controller_method = $requested_route->method;
+        $controller_name::$controller_method();
+    }
+    public static function getRequestedRoute()
+    {
+        $request_method = request::getRequestMethod();
+        $page = request::getPage();
+        $action = request::getAction();
+        $routes = \routes::getRoutes();
+        $foundRoute = NULL;
+        foreach ($routes as $route) {
+            if ($route->page == $page && $route->http_method == $request_method && $route->action == $action) {
+                $foundRoute = $route;
+                break;
+            }
+        }
+        if (is_null($foundRoute)) {
+            controller::getTemplate('notfound');
+            exit;
+        } else {
+            return $foundRoute;
+        }
+    }
+}
+?>
