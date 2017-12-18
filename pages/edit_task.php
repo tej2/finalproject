6a0ae3314eebf055387dde50c767e894156c749a
tej2 <31868523+tej2@users.noreply.github.com>
2017-12-18 18:31:07 -0500

    Update edit_task.php

--- pages/edit_task.php
+++ pages/edit_task.php
@@ -1,6 +1,7 @@
 <!doctype html>
 
 <html>
+	
 <head>
 <style>
 body {
@@ -8,11 +9,14 @@ body {
 }
 </style>
 </head>
-<form action="index.php?page=all_tasks&action=save_task&id=<?php echo $data->id; ?>" method="post" id="updateForm">
- <p><?php print table\displayHTML::generateFormFromOneRecord($data); ?></p>
+	
+<form action="index.php?page=all_tasks&action=update&id=<?php echo $data->id; ?>" method="post" id="updateForm">
+<?php 
+	print table\displayHTML::generateFormFromOneRecord($data); 
+?></p>
 <button type="submit" form="updateForm" value="store" class="edit">Save</button></form>
-  <a href="index.php?page=all_tasks&action=show&id=<?php echo $data->id; ?> "><button class="cancel">Cancel</button></a>
-  <form action="index.php?page=deleted&action=delete&id=<?php echo $data->id; ?> " method="post" id="form1">
-   <button type="submit" form="form1" value="delete" class="delete">Delete</button>
-	</form>
+<a href="index.php?page=all_tasks&action=show&id=<?php echo $data->id; ?> "><button class="cancel">Cancel</button></a>
+<form action="index.php?page=complete&action=delete&id=<?php echo $data->id; ?> " method="post" id="form1">
+<button type="submit" form="form1" value="delete" class="delete">Delete</button>
+</form>
 </html>
