b21a3a28dbcb669cd43c41ea471ade3d83319c8f
tej2 <31868523+tej2@users.noreply.github.com>
2017-12-18 17:55:41 -0500

    Update show_account.php

--- pages/show_account.php
+++ pages/show_account.php
@@ -3,7 +3,7 @@
 <html>
 
 <body>
-<?php print utility\htmlTable::accountValues($data); ?></p>
+<?php print table\displayTable::accountValues($data); ?></p>
   			<hr>
   			<a href="index.php?page=edit_account&action=edit"><button class="edit">Edit  Profile</button></a>
 </html>
