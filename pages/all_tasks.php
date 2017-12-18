bc846f9c913b1019214d2710c4381410c027b9b8
tej2 <31868523+tej2@users.noreply.github.com>
2017-12-18 18:03:26 -0500

    Update all_tasks.php

--- pages/all_tasks.php
+++ pages/all_tasks.php
@@ -2,7 +2,7 @@
 
 <html>
 <?php
-print table/displayTable::genarateTableFromMultiArray($data);
+print table\displayTable::genarateTableFromMultiArray($data);
 ?>
 <a href="index.php?page=create&action=add_task"><button class="edit">Create a Task</button></a>
 
