5f74f3a5865d94cc51743671334fa27de1821679
tej2 <31868523+tej2@users.noreply.github.com>
2017-12-18 18:55:29 -0500

    Update show_task.php

--- pages/show_task.php
+++ pages/show_task.php
@@ -15,7 +15,8 @@ body {
 <button class="btn btn-primary text-center" type="submit" form="form1" value="edit">Edit</button></div>
 </form>
 <form action="index.php?page=tasks&action=delete&id=<?php echo $data->id; ?> " method="post" id="form2" style="float:right;">
-    <button class="btn btn-primary" type="submit" form="form2" value="delete">Delete</button><a href="index.php?page=tasks&action=allOneUser&id=<?php echo $_SESSION["userID"] ?>">Cancel</a></div>
+<button class="btn btn-primary" type="submit" form="form2" value="delete">Delete</button>
+<a href="index.php?page=tasks&action=allOneUser&id=<?php echo $_SESSION["userID"] ?>">Cancel</a></div>
 </form>
 </html>
 </body>
