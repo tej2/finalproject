bfca5d4eac403c320942d9dd42042c934ac95543
tej2 <31868523+tej2@users.noreply.github.com>
2017-12-18 17:07:23 -0500

    Update homepage.php

--- pages/homepage.php
+++ pages/homepage.php
@@ -4,9 +4,9 @@
 <center>
 <div id="container">       
 <form action="index.php?page=accounts&action=login" method="POST">
-  <label for="username"><b><h1>Username:<b></h1></label>
+  <label for="username"><b><h1 style="font-family:verdana;">Username:<b></h1></label>
 <input type="text" placeholder="Enter Username" name="email" required><br><br>
-  <label for="password"><b><h1>Password:</h1></label>
+  <label for="password"><b><h1 style="font-family:verdana;">Password:</h1></label>
 <input type="password" placeholder="Enter Password" name="password" required>
 <div id="lower">
   <p><button type="submit"><b><h2>Login!</h2></button>
