18a02443f56fa174918abfa202127add2852af5b
tej2 <31868523+tej2@users.noreply.github.com>
2017-12-18 16:07:22 -0500

    Update homepage.php

--- pages/homepage.php
+++ pages/homepage.php
@@ -3,22 +3,18 @@
 <body>
 <form action="index.php?page=accounts&action=login" method="POST">
 
-    <div class="container">
-        <label><b><font color="white">Username</b></label>
+        <label><b>Username</b></label>
         <input type="text" placeholder="Enter Username" name="email" required><br><br>
 
         <label><b>Password</b></label></font>
         <input type="password" placeholder="Enter Password" name="pass" required>
         <br><br>
 
-        <button type="submit">  Login  </button>
-    </div>
-    </div>
-
+    <button type="submit">  Login  </button>
 
 
 </form>
-<a href="index.php?page=accounts&action=register">New User?</a>
+<a href="index.php?page=accounts&action=register">sign up here!</a>
 
 </body>
 </html>
