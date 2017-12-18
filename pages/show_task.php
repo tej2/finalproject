2e17af851168805c40fcf781355a38599e465b44
tej2 <31868523+tej2@users.noreply.github.com>
2017-12-18 18:32:59 -0500

    Update show_task.php

--- pages/show_task.php
+++ pages/show_task.php
@@ -12,10 +12,10 @@ body {
 </head>
 <body>
 <form action="index.php?page=tasks&action=edit&id=<?php echo $data->id; ?> " method="post" id="form1" style="float:left;">
-<div class="col-lg-10"><button class="btn btn-primary text-center" type="submit" form="form1" value="edit">Edit</button></div>
+<button class="btn btn-primary text-center" type="submit" form="form1" value="edit">Edit</button></div>
 </form>
 <form action="index.php?page=tasks&action=delete&id=<?php echo $data->id; ?> " method="post" id="form2" style="float:right;">
-<div class="" ><button class="btn btn-primary" type="submit" form="form2" value="delete">Delete</button><a href="index.php?page=tasks&action=allOneUser&id=<?php echo $_SESSION["userID"] ?>">Cancel</a></div>
+    <button class="btn btn-primary" type="submit" form="form2" value="delete">Delete</button><a href="index.php?page=tasks&action=allOneUser&id=<?php echo $_SESSION["userID"] ?>">Cancel</a></div>
 </form>
 </html>
 </body>
