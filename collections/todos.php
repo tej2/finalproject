c27101a99f7177ce034f01013b6f7be7584e5d9b
tej2 <31868523+tej2@users.noreply.github.com>
2017-12-18 15:30:38 -0500

    Update todos.php

--- collections/todos.php
+++ collections/todos.php
@@ -4,7 +4,7 @@ class todos extends database\collection
     protected static $modelName = 'todo';
     public static  function findTasksbyUSERID($userid) {
         $tableName = get_called_class();
-        $sql = 'SELECT * FROM ' . $tableName . ' WHERE ownerid ="' . $userid .'"';
+        $sql = 'SELECT * FROM ' . $tableName . ' WHERE ownerid = ?';
         $recordsSet = self::getResults($sql);
         return $recordsSet;
     }
