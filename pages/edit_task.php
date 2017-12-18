3bc916b30128ed876d2727822f23db96eb42229c
tej2 <31868523+tej2@users.noreply.github.com>
2017-12-18 18:17:18 -0500

    Update edit_task.php

--- pages/edit_task.php
+++ pages/edit_task.php
@@ -1,13 +1,18 @@
 <!doctype html>
 
 <html>
-
+<head>
+<style>
+body {
+    background-color: lightpink;
+}
+</style>
+</head>
 <form action="index.php?page=all_tasks&action=save_task&id=<?php echo $data->id; ?>" method="post" id="updateForm">
-    		<p><?php print table\displayHTML::generateFormFromOneRecord($data); ?></p>
-			<button type="submit" form="updateForm" value="store" class="edit">Save</button></form>
- 			<hr>
-  			<a href="index.php?page=all_tasks&action=show&id=<?php echo $data->id; ?> "><button class="cancel">Cancel</button></a>
-  			<form action="index.php?page=deleted&action=delete&id=<?php echo $data->id; ?> " method="post" id="form1">
-   			 <button type="submit" form="form1" value="delete" class="delete">Delete</button>
+ <p><?php print table\displayHTML::generateFormFromOneRecord($data); ?></p>
+<button type="submit" form="updateForm" value="store" class="edit">Save</button></form>
+  <a href="index.php?page=all_tasks&action=show&id=<?php echo $data->id; ?> "><button class="cancel">Cancel</button></a>
+  <form action="index.php?page=deleted&action=delete&id=<?php echo $data->id; ?> " method="post" id="form1">
+   <button type="submit" form="form1" value="delete" class="delete">Delete</button>
 	</form>
 </html>
