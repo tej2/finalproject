19c87ed544359dc96223bd8e080a86930b7b9836
tej2 <31868523+tej2@users.noreply.github.com>
2017-12-17 20:56:58 -0500

    Update accounts.php

--- collections/accounts.php
+++ collections/accounts.php
@@ -4,9 +4,9 @@ class accounts extends \database\collection
 {
     protected static $modelName = 'account';
   
-    public static  function findUserbyUN($login) {
+    public static  function findUserbyEmail($email) {
         $tableName = get_called_class();
-        $sql = 'SELECT * FROM ' .$tableName . ' WHERE email ="' . $login.'"';
+        $sql = 'SELECT * FROM ' .$tableName . ' WHERE email ="' . $email.'"';
 
         $recordsSet = self::getResults($sql);
         if (is_null($recordsSet)) {
